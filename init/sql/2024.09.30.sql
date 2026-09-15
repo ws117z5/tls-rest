979b2ed1efaad997f52ab40257e15d6c4baf0f46
Vladimir Koroteev <ws117z5@gmail.com>
2026-09-15 15:56:06 -0700

    removed old sql migrations grom git project, now only use public init schema.sql [patch]

--- init/sql/2024.09.30.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE IF EXISTS public.users
-    ADD COLUMN "uuid" uuid DEFAULT uuid_generate_v4();
--- init/sql/2026.09.15.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.translations (key, locale, value) VALUES
-('Execution log', 'ru', 'Журнал выполнения')
-ON CONFLICT (key, locale) DO NOTHING;
